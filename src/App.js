72d8eab4d5d9a81cce89abdbec8841ab6dcb675f
Parth Gupta <parthgupta2510@gmail.com>
2022-07-30 16:01:54 +0530

    carousel trial 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Carousel from './components/Carousel';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Buttons from './components/Buttons';
+import CertiTemplate from './data/CertiTemplate.json'
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #eaeaea;
+`;
+
+const SubWrapper = styled.div`
+width: 50%;
+margin: auto;
+overflow: hidden;
+box-shadow: 0 10px 15px rgba(0, 0, 0, 0.4);
+`;
+
+const App = () => {
+    const [index, setIndex] = useState(0);
+    const [width, setWidth] = useState(0);
+    const [xPosition, setXPosition] = useState(0);
+
+    const images = CertiTemplate
+    console.log(images)
+
+    const handleClickPrev = () => {
+        if (index === 0) return;
+        setIndex(index - 1);
+        setXPosition(xPosition + width);
+    };
+
+    const handleClicknext = () => {
+        if (index === images.length - 1) {
+            setIndex(0);
+            setXPosition(0);
+        } else {
+            setIndex(index + 1);
+            setXPosition(xPosition - width);
+        }
+    };
+    return (
+        <Wrapper className="App">
+            <SubWrapper>
+                <Carousel
+                    images={images}
+                    setWidth={setWidth}
+                    xPosition={xPosition}
+                />
+                <Buttons
+                    handleClickPrev={handleClickPrev}
+                    handleClicknext={handleClicknext}
+                />
+            </SubWrapper>
+        </Wrapper>
+    );
 }
 
-export default App;
+export default App
