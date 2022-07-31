57c94625876a1e6b766cd5ba1d3a714aa2e61aae
Parth Gupta <parthgupta2510@gmail.com>
2022-07-31 14:57:38 +0530

    carousel component and preview component prepared

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ControlledCarousel from './ControlledCarousel';
+
+import data from './data/CertiTemplate.json'
+
+function App() {
+    return (
+        <ControlledCarousel />
+    )
+}
--- src/ControlledCarousel.js
+++ src/ControlledCarousel.js
@@ -3,30 +3,43 @@ import Carousel from 'react-bootstrap/Carousel';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import CertiTemplate from './data/CertiTemplate.json'
+import ExcelPreview from './components/ExcelPreview';
 
 function ControlledCarousel() {
     const [index, setIndex] = useState(0);
+    const [selectedTemplate, setSelectedTemplate] = useState(null);
 
     const handleSelect = (selectedIndex, e) => {
         setIndex(selectedIndex);
     };
 
+    const handleTemplate = (selectedTemplate, e) => {
+        setSelectedTemplate(selectedTemplate.target.id);
+    };
+
+    // console.log(selectedTemplate, "was clicked!")
+
     return (
-        <Carousel activeIndex={index} onSelect={handleSelect}>
-            { CertiTemplate.Templates.map(template => (
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src={template.src}
-                    alt={template.alt}
-                />
-                <Carousel.Caption>
-                    <h3>{template.alt}</h3>
-                    <button className='btn btn-primary' onClick={console.log("was clicked")}>Select</button>
-                </Carousel.Caption>
-            </Carousel.Item>
-            ))}
-        </Carousel>
+        <>
+            <Carousel activeIndex={index} onSelect={handleSelect} interval={null}>
+                {CertiTemplate.Templates.map(template => (
+                    <Carousel.Item key={template.key}>
+                        <img
+                            className="d-block w-100"
+                            id={template.id}
+                            src={template.src}
+                            alt={template.alt}
+                        />
+                        <Carousel.Caption>
+                            <h3>{template.alt}</h3>
+                            <button className='btn btn-primary' id={template.id} onClick={handleTemplate}>Select</button>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                ))}
+            </Carousel>
+            <p className='text-center d-block btn btn-success m-auto'>Certificate {parseInt(selectedTemplate) + 1} selected!</p>
+            <ExcelPreview id={selectedTemplate} />
+        </>
     );
 }
 
